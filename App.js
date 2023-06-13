ed2fc3ebf4279a4555f27d83c7f51cfa719c76f4
saiteja9177 <sirangisaiteja@gmail.com>
2023-06-13 00:53:00 -0700

    React Fragments

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    REACT DEVELOPMENT
-  </h1>
-);
+
 
 //component composition
 const HeadingComponent = () => (
+    <React.Fragment>
     <div id="container">
-    <Title/>
      <h1 className="heading">React Component</h1>
      </div>
+     <div id="container-2"></div>
+     </React.Fragment>
 )
 
+const title = (
+    <h1 className="head" tabIndex="5">
+      REACT DEVELOPMENT
+      <HeadingComponent/>
+    </h1>
+  );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(title);

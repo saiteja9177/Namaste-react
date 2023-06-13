0f71b26300e906a2283b67b427d758a2cdc54a49
saiteja9177 <sirangisaiteja@gmail.com>
2023-06-12 23:18:32 -0700

    JSX Starting

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
-console.log(parent);
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = (
+  <h1 className="head" id="heading">
+    REACT DEVELOPMENT
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
